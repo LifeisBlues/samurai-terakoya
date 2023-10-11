abe2cf9c36e987727235e6d53f1df9366542e71b
LifeisBlues <143789854+LifeisBlues@users.noreply.github.com>
2023-10-11 16:12:51 +0900

    kadai_020追加

---
+++ html-css-advanced-kadai/kadai_014/kadai_020/event.js
@@ -0,0 +1,15 @@
+const Btn = document.getElementById('btn');
+
+const textOne = document.getElementById('text');
+
+Btn,addEventListener('click' , () => {
+    textOne.textContent  = 'ボタンをクリックしました';
+});
+
+
+
+
+
+
+
+
